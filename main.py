72dfd2d2f1f9bb3f055a535fa3e89d92a5ac63ff
Humais55 <mohammadhumais6@gmail.com>
2023-03-03 11:19:38 +0500

    updated

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello world")
+name="Humais"
+last_name="Hashmi"
+program="DSA"
+age=21
+sem=4
+school_name="NCRG-IST"
+print("\t\tMY PROFILE\n")
+print("Name: ",name,"\nLast name: ",last_name,"\nProgram: ",program,"\nAge: ",age,"\nSemester: ",sem,"\nInstitute: ",school_name)
