f936b552e31529a9009f6e2db1ec91c08efda3a4
Humais55 <mohammadhumais6@gmail.com>
2023-03-30 12:10:02 +0500

    Class work of tuple

---
+++ tuple_1.py
@@ -0,0 +1,20 @@
+#Empty tuple
+
+empty_tuple = ()
+empty_tuple = tuple()
+
+#Accessing 
+fruits = ('Water_Melon', 'Pineapple', 'Melon', 'Mango')
+print(fruits[0])
+
+last_fruit =len(fruits)-1
+print(fruits[last_fruit])
+
+print(fruits[-2])
+
+#slicing
+print(fruits[1:3])
+
+#Checking an Item in a Tuple
+
+print('Pineapple' in fruits)
